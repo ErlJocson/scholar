5eb8bceb801a901af3103045c60201567dc79372
ErlJocson <jocsonerl@gmail.com>
2022-06-30 09:35:27 +0800

    added authenticate app

---
+++ authenticate_user/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login_view(request):
+    return
+
+def register_view(request):
+    return
+
+def logout_user(request):
+    return
+
+def change_password(request):
+    return
--- scholars/views.py
+++ scholars/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def index_view(request):
     return render(request, 'index.html', {
         'title':'Home',
     })
+
+def profile_view(request):
+    return 
+
+def change_password(request):
+    return
+
+def logout(request):
+    return
